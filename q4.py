6958f3cb341a81045b3373365737082eeabde329
nbandari1 <133066486+nbandari1@users.noreply.github.com>
2024-02-14 21:36:49 -0500

    Update q4.py

--- q4.py
+++ q4.py
@@ -1,6 +1,8 @@
+#The decorator function defines a nested function called wrapper that calls func
 def decorator(func):
   def wrapper(*args):
     num, lineNum = func(*args)
+    #f strings are used to embed datatypes directly into a string
     print(f"For line no. {lineNum}:")
     print(f"The numbers read: {numbers}")
     print(f"The count of the numbers read: {len(numbers)}")
@@ -8,6 +10,8 @@ def decorator(func):
     print(f"The maximum of the numbers: {max(numbers)}\n\n")
   return wrapper
 
+#this is used to apply the decorator function the stat function, so that whenever stat
+#function is called, it automatically calls the decorator function.
 @decorator
 def stat(nums, lineNum):
   """ This will invoke the decorator function """
@@ -18,8 +22,10 @@ def printStats(t):
   """ Retieves data from text file in lines of numbers and calls decorator function for each line read """
   with open(t, 'r') as file:
     for lineNum, line in enumerate(file, start=1):
+      #this converts the line of numbers to a list of float type numbers
       num = line.split();
       num = list(map(float, num))
+      #this calls the stat and decorator functions for every line
       stat(num, lineNum)
 
 if __name__ == "__main__":
