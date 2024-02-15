554f95a900d199947dd5787c489b5d909107464b
nbandari1 <133066486+nbandari1@users.noreply.github.com>
2024-02-14 20:35:17 -0500

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,1 +1,15 @@
 def wordCount(t):
+  """ Retrieves data from a text file and returns a dictionary """
+  dict = {}
+  with open (t, 'r') as f:
+    for lineNum, line in enumerate(file, start=1):
+      words = line.split()
+      for word in words:
+        if word in dict:
+          dict[word].append(lineNum)
+        else:
+          dict[word] = [lineNum]
+  return dict
+
+if __name__ == "__main__":
+  print(wordCount('test2.txt'))
