3c54c389a66c4b51ea8a43294ae9d47db4d96c75
nbandari1 <133066486+nbandari1@users.noreply.github.com>
2024-02-14 20:40:44 -0500

    Update q4.py

--- q4.py
+++ q4.py
@@ -1,1 +1,26 @@
+def decorator(func):
+  def wrapper(*args):
+    num, lineNum = func(*args)
+    print(f"For line no. {lineNum}:")
+    print(f"The numbers read: {numbers}")
+    print(f"The count of the numbers read: {len(numbers)}")
+    print(f"The average of the numbers: {round(sum(numbers)/len(numbers),2)}")
+    print(f"The maximum of the numbers: {max(numbers)}\n\n")
+  return wrapper
+
+@decorator
+def stat(nums, lineNum):
+  """ This will invoke the decorator function """
+  return nums, lineNum
+
+
 def printStats(t):
+  """ Retieves data from text file in lines of numbers and calls decorator function for each line read """
+  with open(t, 'r') as file:
+    for lineNum, line in enumerate(file, start=1):
+      num = line.split();
+      num = list(map(float, num))
+      stat(num, lineNum)
+
+if __name__ == "__main__":
+printStats('test3.txt')
