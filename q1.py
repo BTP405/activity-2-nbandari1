ce30d1a4d84d7cf4f3d6005ee174bf152ca48f9b
nbandari1 <133066486+nbandari1@users.noreply.github.com>
2024-02-14 21:18:19 -0500

    Update q1.py

--- q1.py
+++ q1.py
@@ -4,6 +4,9 @@ def checkPrime(n):
   #number has to be greater than 2
   if n < 2:
     return False
+  #n*0.5 calculates half of n. For example if n is 10, then n*0.5 is 5. The +1 is included 
+  #for rounding down any decimal number to the nearest integer. This condition calculates the 
+  #upper limit for the loop.
   for i in range (2, int(n*0.5 + 1)):
     if n%i == 0:
       return False
@@ -17,6 +20,7 @@ def getPrimeNumbers(n):
       num.append(i)
   return num
 
-
+#This ensures that the code runs when the script is executed directly and not imported
+#as a module.
 if __name__ == "__main__":
   print(getPrimeNumbers(23))
